d252d54b0e4f6cebf3e3510347a66a91d6b717f1
Quinn Gale <q.gale@live.com>
2022-10-15 06:06:54 +0000

    Added footer, and a notifications section

--- inc/doc.php
+++ inc/doc.php
@@ -1,5 +1,6 @@
 <?php
 $site_title = "Gas Tracker";
+$flash = array();
 
 function site_head($page_title)
 {
@@ -12,59 +13,122 @@ function site_head($page_title)
         <link rel="stylesheet" href="/css/bulma.css">
         
         <script src="/js/bulma.js"></script>
+        <script src="https://kit.fontawesome.com/3587f0af61.js" crossorigin="anonymous"></script>
     </head>
+
     EOT;
 }
 
-function site_header()
+function site_nav()
 {
     echo <<<EOT
-    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="/">
-                Gas Tracker
-            </a>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    Gas Tracker
+                </a>
 
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="main-nav">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            </a>
-        </div>
-
-        <div id="main-nav" class="navbar-menu">
-            <div class="navbar-start">
-                <a class="navbar-item">
-                    Documentation
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="main-nav">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
                 </a>
             </div>
 
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                    <a class="button is-link">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
+            <div id="main-nav" class="navbar-menu">
+                <div class="navbar-start">
+                    <!--- <a class="navbar-item">
+                        Documentation
+                    </a> --->
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                        <a href="/account/sign-up" class="button is-light">
+                            Sign up
+                        </a>
+                        <a class="button is-primary">
+                            Log in
+                        </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+
+    EOT;
+}
+
+function site_footer()
+{
+    echo <<<EOT
+        <footer class="footer">
+            <div class="container">
+                <div class="level">
+                    <div class="level-item">
+                        <p>
+                            Created by <a href="https://quinngale.com" title="Quinn Gale">Quinn Gale</a>
+                        </p>
+                    </div>
+                    <div class="level-item">
+                        <a href="https://github.com/gabeauim/gas-tracker" class="button">
+                            <span class="icon">
+                                <i class="fab fa-github"></i>
+                            </span>
+                            <span>GitHub</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
     EOT;
 }
 
 function site_top($page_title)
 {
+    global $flash;
+
     echo <<<EOT
+    <!doctype HTML>
     <html>
+
     EOT;
     site_head($page_title);
 
     echo <<<EOT
     <body>
+    
     EOT;
-    site_header();
+    site_nav();
+
+    if (!empty($flash)) {
+        echo <<<EOT
+                <div class="container">
+        EOT;
+
+        foreach ($flash as $message) {
+            echo <<<EOT
+                        <div class="notification">
+                            <button class="delete"></button>
+                            $message
+                        </div>
+            EOT;
+        }
+
+        echo <<<EOT
+                </div>
+        EOT;
+    }
+}
+
+function site_bottom()
+{
+    site_footer();
+
+    echo <<<EOT
+    </body>
+    </html>
+    EOT;
 }
